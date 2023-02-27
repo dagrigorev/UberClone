0ae2e7d4ee86d5f6be08362c4121bd144fddd227
Dmitry Grigorev <jabberwooky@icloud.com>
2023-02-27 11:59:09 +0300

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Dmitry Grigorev on 27.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
